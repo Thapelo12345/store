0b522fddf32faa590961e1ecc18462f912b7ef1d
Thapelo12345 <79000570+Thapelo12345@users.noreply.github.com>
2024-12-19 17:28:34 +0200

    fixed play

--- src/Landing pages/screen/screen-functions.js
+++ src/Landing pages/screen/screen-functions.js
@@ -1,12 +1,6 @@
 import $ from 'jquery';
 import { itemList } from '../Items';
 
-// var itemList
-// function getData(data){
-//     itemList = data
-// }
-
-// fetch("../clothingItems.json").then((res)=> getData(res.json()))
 
 var p1 = '#pic1', p2 = '#pic2', p3 = '#pic3', p4 = '#pic4'
 var onView = 'p2'
@@ -99,169 +93,146 @@ function changePicture(){
     }//end of else if
  
 }//end of change pic function
-
-function slide(){
-
-
-    if(onView === 'p1'){
-//hidde p4 open p3
+function slide() {
+    if (onView === 'p1') {
+        //hide p4 open p3
         $(p4).css({
             transform: 'translateX(-337%) scale(.1)',
             zIndex: '-1'
-        })
+        });
 
         $(p1).css({
             transform: 'translateX(0) scale(1)'
-        })
+        });
 
         $(p2).css({
             transform: 'translateX(40%) scale(1.5)'
-        })
+        });
 
         $(p3).css({
             visibility: 'visible',
             zIndex: '0',
             transform: 'translateX(120%) scale(1)'
-        })
+        });
 
-        let pause = setTimeout(()=>{
+        applyCSSWithDelay(() => {
             $(p4).css({
                 visibility: 'hidden'
-            })
-            clearTimeout(pause)
-        }, 20)//visibility
-
-        let puase1 = setTimeout(()=>{
-
+            });
+        }, 20).then(() => applyCSSWithDelay(() => {
             $(p4).css({
                 transform: 'translatex(9%) scale(.1)',
                 zIndex: '-1'
-            })
-            clearInterval(puase1)
-        }, 2700)//end of motion
+            });
+        }, 2700));
 
-        onView = 'p2'
-    }//end of if
-
-    else if(onView === 'p2'){
-//hide p1 open p4
+        onView = 'p2';
+    } else if (onView === 'p2') {
+        //hide p1 open p4
         $(p1).css({
             transform: 'scale(.1)'
-        })
+        });
 
         $(p2).css({
             transform: 'translateX(-113%) scale(1)'
-        })
+        });
 
         $(p3).css({
-            transform: 'translateX(-52%) scale(1.5)',
-        })
+            transform: 'translateX(-52%) scale(1.5)'
+        });
 
         $(p4).css({
             transform: 'translateX(9%) scale(1)',
             zIndex: '0',
             visibility: 'visible'
-        })
+        });
 
-        let pause = setTimeout(()=>{
+        applyCSSWithDelay(() => {
             $(p1).css({
                 visibility: 'hidden'
-            })
-
-            clearTimeout(pause)
-        }, 20)//set visibility
-
-        let pause1 = setTimeout(()=>{
+            });
+        }, 20).then(() => applyCSSWithDelay(() => {
             $(p1).css({
                 zIndex: '-1',
                 transform: 'translateX(333%) scale(.1)'
-            })
-            clearTimeout(pause1)
-        }, 2700)//end of motion
+            });
+        }, 2700));
 
-        onView = 'p3'
-    }//end of else if
-
-    else if(onView === 'p3'){
-//hide p2 open  p1 
+        onView = 'p3';
+    } else if (onView === 'p3') {
+        //hide p2 open p1
         $(p1).css({
             visibility: 'visible',
             zIndex: '0',
             transform: 'translateX(333%) scale(1)'
-        })
+        });
         $(p2).css({
             transform: 'translateX(-113%) scale(.1)'
-        })
+        });
 
         $(p3).css({
             transform: 'translateX(-224%) scale(1)'
-        })
+        });
 
         $(p4).css({
             transform: 'translateX(-162%) scale(1.5)'
-        })
+        });
 
-        let puase = setTimeout(()=>{
+        applyCSSWithDelay(() => {
             $(p2).css({
                 visibility: 'hidden'
-            })
-
-            clearTimeout(puase)
-        }, 20)//visibility 
-
-        let pause1 = setTimeout(()=>{
+            });
+        }, 20).then(() => applyCSSWithDelay(() => {
             $(p2).css({
                 zIndex: '-1',
                 transform: 'translateX(222%) scale(.1)'
-            })
+            });
+        }, 2700));
 
-            clearTimeout(pause1)
-        }, 2700)//end of motion
-
-        onView = 'p4'
-    }//end of else if
-
-    else if(onView === 'p4'){
-//hide p3 open p2     
+        onView = 'p4';
+    } else if (onView === 'p4') {
+        //hide p3 open p2
         $(p3).css({
             transform: 'translateX(-224%) scale(.1)'
-        })
+        });
 
         $(p4).css({
             transform: 'translateX(-337%) scale(1)'
-        })
+        });
 
         $(p1).css({
             transform: 'translateX(170%) scale(1.5)'
-        })
+        });
 
         $(p2).css({
             visibility: 'visible',
             zIndex: '0',
-             transform: 'translateX(222%) scale(1)'
-        })
+            transform: 'translateX(222%) scale(1)'
+        });
 
-        let puase = setTimeout(()=>{
-
+        applyCSSWithDelay(() => {
             $(p3).css({
                 visibility: 'hidden'
-            })
-            clearTimeout(puase)
-        },20)//visibility
-
-        let puase1 = setTimeout(()=>{
-
+            });
+        }, 20).then(() => applyCSSWithDelay(() => {
             $(p3).css({
                 zIndex: '-1',
                 transform: 'translateX(111%) scale(.1)'
-            })
-            clearTimeout(puase1)
+            });
+        }, 2700));
 
-        },2700)//end of motion
+        onView = 'p1';
+    }
+}
 
-        onView = 'p1'
-    }//end of else if
+function applyCSSWithDelay(action, delay) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            action();
+            resolve();
+        }, delay);
+    });
+}
 
-}//end of slide funt
 
 export {slide, onView, indexer1, indexer2, indexer3, indexer4, changePicture }
